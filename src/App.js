0d319c43a72e24ff72da93156da5187012c0d675
nghia64 <nguyentrongn168@gmail.com>
2023-01-13 16:02:40 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <div className="top-header"></div>
-        <div className="bottom-header"></div>
-      </div>
-      <div className="main">
-        <div className="main-header"></div>
-        <div className="main-body">
-          <div className="sidebar-left"></div>
-          <div className="sidebar-right"></div>
+      <div className="content">
+        <div className="header">
+          <div className="top-header">
+            <div>Tai ung dung</div>
+            <div>Tai ung dung</div>
+            <div>Tai ung dung</div>
+          </div>
+          <div className="bottom-header">
+            <div className="logo"></div>
+            <div className="form-search">
+              <div></div>
+              <form>
+                <input />
+                <button></button>
+              </form>
+            </div>
+            <div className="sign-in">
+              <div></div>
+              <button>Dang Nhap</button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="footer">
-        <div className="footer1"></div>
-        <div className="footer2"></div>
-        <div className="footer3"></div>
+        <div className="main">
+          <div className="main-header">
+            <div></div>
+            <div></div>
+          </div>
+          <div className="main-body">
+            <div className="sidebar-left">
+              <div></div>
+            </div>
+            <div className="sidebar-right">
+              <div className="sort"></div>
+              <div className="ads-products"></div>
+              <div className="all-products"></div>
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="footer-ads">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="footer-about">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="footer-contact">
+            <div></div>
+            <div></div>
+          </div>
+          <div className="footer-top-search">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </div>
     </div>
   );
