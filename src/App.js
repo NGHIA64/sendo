9300e4d8aa9a9dfcd092e71e90deb9952c6673f7
nghia64 <nguyentrongn168@gmail.com>
2023-01-13 15:38:19 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="header">
+        <div className="top-header"></div>
+        <div className="bottom-header"></div>
+      </div>
+      <div className="main">
+        <div className="main-header"></div>
+        <div className="main-body">
+          <div className="sidebar-left"></div>
+          <div className="sidebar-right"></div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer1"></div>
+        <div className="footer2"></div>
+        <div className="footer3"></div>
+      </div>
     </div>
   );
 }
