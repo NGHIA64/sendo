7c8740c55fd249a366e5ca6830c57a8f87ee6b43
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-16 11:39:20 +0700

    css start body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import logo from './img/logo.svg'
 import menu from './img/menu.svg'
+import cart from './img/cart.svg'
 function App() {
   return (
     <div className="App">
@@ -21,20 +22,38 @@ function App() {
         <div className="bottom-header">
           <div className="content">
             <div className="logo">
-              <img src={logo} height='48px' width='100%' />
+              <img src={logo} height="48px" width="100%" />
             </div>
             <div className="form-search">
-              <div>
-                <img src={menu}/>
+              <div className="menu">
+                <img src={menu} />
               </div>
-              <form>
-                <input />
-                <button></button>
+              <form className="search-form">
+                <input className="input-search" value='giày nam'/>
+                <button className="btn-search">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    xlink="http://www.w3.org/1999/xlink"
+                    className="svg-search"
+                  >
+                    <path
+                      d="M10 2a8 8 0 0 1 6.32 12.905l5.387 5.388-1.414 1.414-5.388-5.386A8 8 0 1 1 10 2Zm0 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12Z"
+                      fill="#445058"
+                      fill-rule="nonzero"
+                    ></path>
+                  </svg>
+                </button>
               </form>
             </div>
             <div className="sign-in">
-              <div></div>
-              <button>Dang Nhap</button>
+              <div className="cart">
+                <img src={cart}/>
+              </div>
+              <button className="btn-sign-in">Đăng nhập</button>
             </div>
           </div>
         </div>
@@ -42,8 +61,13 @@ function App() {
       <div className="main">
         <div className="content">
           <div className="main-header">
-            <div></div>
-            <div></div>
+            <div className="main-header-1">
+              <a>Sendo.vn</a>
+              <span className="text-[#6f787e]"> / Kết quả tìm kiếm</span>
+            </div>
+            <div className="main-header-2">
+              <span>giày nam</span>
+            </div>
           </div>
           <div className="main-body">
             <div className="sidebar-left">
