ec00e29188d386e9dbe4f3722d0322962f22ac2f
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-16 23:37:47 +0700

    product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import logo from './img/logo.svg'
-import menu from './img/menu.svg'
-import cart from './img/cart.svg'
+import logo from "./img/logo.svg";
+import menu from "./img/menu.svg";
+import cart from "./img/cart.svg";
 function App() {
   return (
     <div className="App">
@@ -283,6 +283,25 @@ function App() {
                             <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
                             <div className="name">
                               Giày Sneaker VNS Old Màu Đen Nam
+                            </div>
+                            <div>175.000đ</div>
+                            <div>Đã bán 125</div>
+                            <div>
+                              <span>
+                                4.6/5{" "}
+                                <svg
+                                  width="16"
+                                  height="16"
+                                  aria-hidden="true"
+                                  fill="yellow"
+                                  viewBox="0 0 20 20"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <title>First star</title>
+                                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                </svg>
+                              </span>
+                              <div>TP.HCM</div>
                             </div>
                           </div>
                         </div>
