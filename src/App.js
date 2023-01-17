289179aed1d08e95cf47e874eee85b5148851874
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-17 16:35:25 +0700

    done ui

--- src/App.js
+++ src/App.js
@@ -835,6 +835,7 @@ function App() {
                     </div>
                   </a>
                 </div>
+
                 <div className="ads-product-item main-product">
                   <a className="main-product">
                     <div>
@@ -874,46 +875,6 @@ function App() {
                     </div>
                   </a>
                 </div>
-                <div className="ads-product-item main-product">
-                  <a className="main-product">
-                    <div>
-                      <div className="img-ads-product">
-                        <img src="https://media3.scdn.vn/img4/2021/02_20/a4u8ix3clwWA4csAB7V7_simg_de2fe0_250x250_maxb.jpg" />
-                      </div>
-                      <div className="deitail-product">
-                        <span className="branch-ads">
-                          <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />{" "}
-                          Giày Sneaker VNS Old Màu Đen Nam
-                        </span>
-                        <div className="old-price"></div>
-                        <div className="price">175.000đ</div>
-                        <div className="tra-gop">
-                          <img src="https://media3.scdn.vn/img4/2022/06_24/V5PHsdxRbMf35yH1KO0h.png" />
-                          Trả góp Kredivo
-                        </div>
-                        <div className="numbers-sale">Đã bán 125</div>
-                        <div className="star">
-                          <span>
-                            4.6/5{" "}
-                            <svg
-                              width="12"
-                              height="12"
-                              aria-hidden="true"
-                              fill="#ffc600"
-                              viewBox="0 0 20 20"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <title>First star</title>
-                              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                            </svg>
-                          </span>
-                          <div className="adress">TP.HCM</div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-                
               </div>
               <div className="footer-sidebar">
                 <button className="btn-xem-them-product">Xem thêm</button>
@@ -925,24 +886,174 @@ function App() {
       <div className="footer">
         <div className="content">
           <div className="footer-ads">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <a className="footer-ads-item">
+              <img src="https://media3.scdn.vn/img4/2020/12_16/gJwXr6FFZKZCGKWaz4RB.png" />
+              <div className="title-footer-ads">Siêu nhiều hàng tốt</div>
+              <div className="detail-footer-ads">
+                Cần gì cũng có 26 ngành hàng & 10 triệu sản phẩm
+              </div>
+            </a>
+            <a className="footer-ads-item">
+              <img src="https://media3.scdn.vn/img4/2020/12_16/EfZWQVfV6nQzu2vMmnwC.png" />
+              <div className="title-footer-ads">Siêu yên tâm</div>
+              <div className="detail-footer-ads">Miễn phí đổi trả 48h</div>
+            </a>
+            <a className="footer-ads-item">
+              <img src="https://media3.scdn.vn/img4/2020/12_16/j5C6IQz7gIXPgjFJxmRz.png" />
+              <div className="title-footer-ads">Siêu tiện lợi</div>
+              <div className="detail-footer-ads">
+                Mang thế giới mua sắm của Sendo trong tầm tay bạn
+              </div>
+            </a>
+            <a className="footer-ads-item">
+              <img src="https://media3.scdn.vn/img4/2020/12_16/7AJFQGQ5qvS7gGOz8P7a.png" />
+              <div className="title-footer-ads">Siêu tiết kiệm</div>
+              <div className="detail-footer-ads">
+                Giá hợp lý, vừa túi tiền. Luôn có nhiều chương trình khuyến mãi
+              </div>
+            </a>
           </div>
-          <div className="footer-about">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        </div>
+        <div className="footer-about">
+          <div className="content">
+            <div className="main-about">
+              <div className="about-item">
+                <div className="title-about">VỀ CHÚNG TÔI</div>
+                <div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                </div>
+              </div>
+              <div className="about-item">
+                <div className="title-about">VỀ CHÚNG TÔI</div>
+                <div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                </div>
+              </div>
+              <div className="about-item">
+                <div className="title-about">VỀ CHÚNG TÔI</div>
+                <div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                </div>
+              </div>
+              <div className="about-item">
+                <div className="title-about">VỀ CHÚNG TÔI</div>
+                <div>
+                  <div className="detail-about">Giới thiệu Sendo.vn</div>
+                  <div className="list-img-about">
+                    <img
+                      src="https://media3.scdn.vn/img4/2020/12_16/5lUTWdk3DXr8nlC9MDII.png"
+                      className="img-about"
+                    />
+                    <img
+                      src="https://media3.scdn.vn/img4/2021/10_26/0ZARLASzVrfL92924rzW.png"
+                      className="img-about"
+                    />
+                    <img
+                      src="https://media3.scdn.vn/img4/2021/03_19/AMV086JNpEbm4OGAvVng.png"
+                      className="img-about"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="footer-contact">
-            <div></div>
-            <div></div>
+        </div>
+        <div className="footer-contact">
+          <div className="content">
+            <div className="main-contact">
+              <div className="left-contact">
+                <div className="name-company">
+                  Công ty Cổ phần Công nghệ Sen Đỏ, thành viên của Tập đoàn FPT
+                </div>
+                <div className="detail-company">
+                  Số ĐKKD: 0312776486 - Ngày cấp: 13/05/2014, được sửa đổi lần
+                  thứ 20, ngày 26/04/2022.
+                </div>
+                <div className="detail-company">
+                  Cơ quan cấp: Sở Kế hoạch và Đầu tư TPHCM.
+                </div>
+                <div className="detail-company">
+                  Địa chỉ: Tầng 5, Tòa nhà A, Vườn Ươm Doanh Nghiệp, Lô D.01,
+                  Đường Tân Thuận, Khu chế xuất Tân Thuận, Phường Tân Thuận
+                  Đông, Quận 7, Thành phố Hồ Chí Minh, Việt Nam.
+                </div>
+                <div className="detail-company">Email: lienhe@sendo.vn</div>
+                <div className="detail-company">
+                  <div className="policy">
+                    <img src="https://media3.scdn.vn/img4/2020/12_16/XhpGDnvWqrlKeHLst3aS.png" />
+                    <img src="https://media3.scdn.vn/img4/2020/12_16/h6lEMGIAt4Uapd0Mls34.png" />
+                  </div>
+                </div>
+              </div>
+              <div className="right-contact">
+                <div className="title-contact">
+                  Đăng ký nhận bản tin ưu đãi khủng từ Sendo
+                </div>
+                <form className="form-contact">
+                  <input
+                    placeholder="Email của bạn là"
+                    type="text"
+                    inputMode="email"
+                  />
+                  <button>Đăng ký</button>
+                </form>
+              </div>
+            </div>
           </div>
-          <div className="footer-top-search">
-            <div></div>
-            <div></div>
+        </div>
+        <div className="footer-top-search">
+          <div className="content">
+            <div className="main-search-footer">
+              <div className="search-title">
+                <span>TOP TÌM KIẾM</span>
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  xlink="http://www.w3.org/1999/xlink"
+                  class="d7ed-SwZDZ2"
+                >
+                  <path
+                    fill="#6F787E"
+                    fill-rule="nonzero"
+                    d="M12 13.214 17.524 8 19 9.393 12 16 5 9.393 6.476 8z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="key-word">
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+                <span>Đồng Hồ</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
