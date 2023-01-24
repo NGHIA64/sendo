18b9d32f24e1ae82faad3097a20a10c3123c7a25
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-25 00:25:20 +0700

    change favicon

--- src/App.js
+++ src/App.js
@@ -7,13 +7,51 @@ function App() {
     <div className="App">
       <div className="header">
         <div className="top-header">
-            <div className="content top-header-item">
-              <div>Tải ứng dụng</div>
-              <div>Chăm sóc khách hàng</div>
-              <div>Kiểm tra đơn hàng</div>
+          <div className="content top-header-item section">
+            <div>Tải ứng dụng</div>
+            <div>Chăm sóc khách hàng</div>
+            <div>Kiểm tra đơn hàng</div>
+          </div>
+        </div>
+        <div className="bottom-header">
+          <div className="bottom-header-item content section">
+            <div className="logo">
+              <img src={logo} height="48px" width="100%" />
+            </div>
+            <div className="form-search">
+              <div className="menu">
+                <img src={menu} />
+              </div>
+              <form className="search-form">
+                <input className="input-search" value="giày nam" />
+                <button className="btn-search">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    xlink="http://www.w3.org/1999/xlink"
+                    className="svg-search"
+                  >
+                    <path
+                      d="M10 2a8 8 0 0 1 6.32 12.905l5.387 5.388-1.414 1.414-5.388-5.386A8 8 0 1 1 10 2Zm0 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12Z"
+                      fill="#445058"
+                      fill-rule="nonzero"
+                    ></path>
+                  </svg>
+                </button>
+              </form>
+            </div>
+            <div className="sign-in">
+              <div className="cart">
+                <img src={cart} />
+              </div>
+              <button className="btn-sign-in">Đăng nhập</button>
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
