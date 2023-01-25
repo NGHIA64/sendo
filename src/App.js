e96177672448ecd4b3587fe8482033728b357c4f
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-26 06:32:19 +0700

    done ads-product

--- src/App.js
+++ src/App.js
@@ -259,24 +259,6 @@ function App() {
                     <span>Tiêu chuẩn</span>
                   </div>
                 </div>
-                <div className="footer-filter">
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    version="1.1"
-                    xlink="http://www.w3.org/1999/xlink"
-                    class="d7ed-SwZDZ2 d7ed-w34diS"
-                  >
-                    <path
-                      fill="#3f4b53"
-                      fill-rule="nonzero"
-                      d="M13 11h9v2h-9v9h-2v-9H2v-2h9V2h2z"
-                    ></path>
-                  </svg>
-                  <span>Xem thêm</span>
-                </div>
               </div>
               <hr />
               <div className="item-filter">
@@ -374,24 +356,6 @@ function App() {
                     <span>Shop uy tín</span>
                   </div>
                 </div>
-                <div className="footer-filter">
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    version="1.1"
-                    xlink="http://www.w3.org/1999/xlink"
-                    class="d7ed-SwZDZ2 d7ed-w34diS"
-                  >
-                    <path
-                      fill="#3f4b53"
-                      fill-rule="nonzero"
-                      d="M13 11h9v2h-9v9h-2v-9H2v-2h9V2h2z"
-                    ></path>
-                  </svg>
-                  <span>Xem thêm</span>
-                </div>
               </div>
               <hr />
               <div className="item-filter">
@@ -435,7 +399,7 @@ function App() {
                     <span>SALE RỘN RÀNG</span>
                   </div>
                   <div className="body-filter-item">
-                  <svg
+                    <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
@@ -497,10 +461,193 @@ function App() {
                 <span>Sắp xếp theo:</span>
                 <div className="block">
                   <div>Đề cử</div>
-                  <svg width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" version="1.1" xlink="http://www.w3.org/1999/xlink" class="d7ed-SwZDZ2 d7ed-OBpTEp"><path fill="#6F787E" fill-rule="nonzero" d="M12 13.214 17.524 8 19 9.393 12 16 5 9.393 6.476 8z"></path></svg>
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    xlink="http://www.w3.org/1999/xlink"
+                    class="d7ed-SwZDZ2 d7ed-OBpTEp"
+                  >
+                    <path
+                      fill="#6F787E"
+                      fill-rule="nonzero"
+                      d="M12 13.214 17.524 8 19 9.393 12 16 5 9.393 6.476 8z"
+                    ></path>
+                  </svg>
                 </div>
               </div>
-              
+              <div className="ads-product">
+                <span>Sản phẩm được tài trợ</span>
+                <div>
+                  <a className="product-item">
+                    <img src="https://media3.scdn.vn/img4/2022/06_09/yg9JxBY4MHB5UQw2nGeb_simg_de2fe0_250x250_maxb.jpg" />
+                    <div className="info-product">
+                      <div className="branch">
+                        <img src="https://media3.scdn.vn/img4/2021/10_21/mFcIndYzGOkBpNH6w5oN.png" />
+                      </div>
+                      <div className="name">
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>Giày nam họa tiết nổi bật</span>
+                      </div>
+                      <div className="sale">
+                        <span>150.000đ</span>
+                        <div>-48%</div>
+                      </div>
+                      <div className="price">79.000đ</div>
+                      <div className="sold">Đã bán 331</div>
+                      <div className="quality">
+                        <div><span>4.6/5</span>
+                        <svg
+                          width="12"
+                          height="12"
+                          aria-hidden="true"
+                          fill="#ffc600"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <title>First star</title>
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg></div>
+                        <div className="adress">TP.HCM</div>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="product-item">
+                    <img src="https://media3.scdn.vn/img4/2022/06_09/yg9JxBY4MHB5UQw2nGeb_simg_de2fe0_250x250_maxb.jpg" />
+                    <div className="info-product">
+                      <div className="branch">
+                        <img src="https://media3.scdn.vn/img4/2021/10_21/mFcIndYzGOkBpNH6w5oN.png" />
+                      </div>
+                      <div className="name">
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>Giày nam họa tiết nổi bật</span>
+                      </div>
+                      <div className="sale">
+                        <span>150.000đ</span>
+                        <div>-48%</div>
+                      </div>
+                      <div className="price">79.000đ</div>
+                      <div className="sold">Đã bán 331</div>
+                      <div className="quality">
+                        <div><span>4.6/5</span>
+                        <svg
+                          width="12"
+                          height="12"
+                          aria-hidden="true"
+                          fill="#ffc600"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <title>First star</title>
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg></div>
+                        <div className="adress">TP.HCM</div>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="product-item">
+                    <img src="https://media3.scdn.vn/img4/2022/06_09/yg9JxBY4MHB5UQw2nGeb_simg_de2fe0_250x250_maxb.jpg" />
+                    <div className="info-product">
+                      <div className="branch">
+                        <img src="https://media3.scdn.vn/img4/2021/10_21/mFcIndYzGOkBpNH6w5oN.png" />
+                      </div>
+                      <div className="name">
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>Giày nam họa tiết nổi bật</span>
+                      </div>
+                      <div className="sale">
+                        <span>150.000đ</span>
+                        <div>-48%</div>
+                      </div>
+                      <div className="price">79.000đ</div>
+                      <div className="sold">Đã bán 331</div>
+                      <div className="quality">
+                        <div><span>4.6/5</span>
+                        <svg
+                          width="12"
+                          height="12"
+                          aria-hidden="true"
+                          fill="#ffc600"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <title>First star</title>
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg></div>
+                        <div className="adress">TP.HCM</div>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="product-item">
+                    <img src="https://media3.scdn.vn/img4/2022/06_09/yg9JxBY4MHB5UQw2nGeb_simg_de2fe0_250x250_maxb.jpg" />
+                    <div className="info-product">
+                      <div className="branch">
+                        <img src="https://media3.scdn.vn/img4/2021/10_21/mFcIndYzGOkBpNH6w5oN.png" />
+                      </div>
+                      <div className="name">
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>Giày nam họa tiết nổi bật</span>
+                      </div>
+                      <div className="sale">
+                        <span>150.000đ</span>
+                        <div>-48%</div>
+                      </div>
+                      <div className="price">79.000đ</div>
+                      <div className="sold">Đã bán 331</div>
+                      <div className="quality">
+                        <div><span>4.6/5</span>
+                        <svg
+                          width="12"
+                          height="12"
+                          aria-hidden="true"
+                          fill="#ffc600"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <title>First star</title>
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg></div>
+                        <div className="adress">TP.HCM</div>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="product-item">
+                    <img src="https://media3.scdn.vn/img4/2022/06_09/yg9JxBY4MHB5UQw2nGeb_simg_de2fe0_250x250_maxb.jpg" />
+                    <div className="info-product">
+                      <div className="branch">
+                        <img src="https://media3.scdn.vn/img4/2021/10_21/mFcIndYzGOkBpNH6w5oN.png" />
+                      </div>
+                      <div className="name">
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>Giày nam họa tiết nổi bật</span>
+                      </div>
+                      <div className="sale">
+                        <span>150.000đ</span>
+                        <div>-48%</div>
+                      </div>
+                      <div className="price">79.000đ</div>
+                      <div className="sold">Đã bán 331</div>
+                      <div className="quality">
+                        <div><span>4.6/5</span>
+                        <svg
+                          width="12"
+                          height="12"
+                          aria-hidden="true"
+                          fill="#ffc600"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <title>First star</title>
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg></div>
+                        <div className="adress">TP.HCM</div>
+                      </div>
+                    </div>
+                  </a>                 
+                </div>
+              </div>
             </div>
           </div>
         </div>
